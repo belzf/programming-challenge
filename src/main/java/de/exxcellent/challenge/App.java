ec87b92c5904a6f041b9017fbed62d6414b6b8b8
Felix Belz <st155595@stud.uni-stuttgart.de>
2023-10-14 19:41:51 +0200

    Update App to print out the solution of the weather challenge

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,10 +1,15 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.weather.WeatherCalculator;
+
+import java.nio.file.Path;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
  *
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
+ * @author Felix Belz
  */
 public final class App {
 
@@ -14,10 +19,24 @@ public final class App {
      */
     public static void main(String... args) {
 
-        // Your preparation code …
+        if (args.length != 1) {
+            System.out.println("You must provide a path to a weather data csv file!");
+            return;
+        }
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+        Path weatherCSVPath = Path.of(args[0]);
+
+        try {
+            WeatherCalculator weatherCalculator = new WeatherCalculator(weatherCSVPath);
+            String dayWithSmallestTempSpread = weatherCalculator.getDayWithSmallestSpread();
+            System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println("The weather data could not be imported. Are you sure '" + weatherCSVPath + "' points " +
+                    "to a valid weather data CSV file?");
+        } catch (Exception e) {
+            System.out.println("Something went wrong. Please try again.");
+        }
 
         String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
